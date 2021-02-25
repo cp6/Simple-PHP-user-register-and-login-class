0c706c53ca8b50209fb0be26e493ed6457010957
corbpie <cp6@users.noreply.github.com>
2021-02-25 15:01:32 +1100

    Fixed mis-assigned variable
    Fixed mis-assigned variable

--- activate.php
+++ activate.php
@@ -3,7 +3,7 @@ require_once('class.php');
 if (isset($_GET['key'])) {
     $activate = new doRegisterAttempt('', '', '');
     $verify = $activate->verifyAccount($_GET['key']);
-    ($verify) ? $verify->outputString("Account activated") : $verify->outputString("Key is invalid");
+    ($verify) ? $activate->outputString("Account activated") : $activate->outputString("Key is invalid");
 } else {//No key
     $cc = new configAndConnect();
     $cc->outputString("Key is required");
